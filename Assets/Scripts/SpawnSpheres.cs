1bff5956a9b2cc932d76ecab7693c663f59c1aa4
Nathan Koch <nathanrkoch@gmail.com>
2020-05-14 09:00:37 -0400

    much improved lighting and environment

--- Assets/Scripts/SpawnSpheres.cs
+++ Assets/Scripts/SpawnSpheres.cs
@@ -7,33 +7,33 @@ public class SpawnSpheres : MonoBehaviour
 
     public GameObject sphere;
     public Transform parent;
+    public Transform floor;
+
     public float overallSize = 5.0f;
     public float minSize = 1.0f;
+    public float initialDelay = 2.0f;
     public float rate = 5.0f;
     public int maxSpheres = 11;
+    public int maxAttemptsAtValidPosition = 5;
+    public float paddingAboveFloor = 0.05f;
+
     private int numSpheres = 0;
+    private Renderer sphereRenderer;
 
     void Start()
     {
-        InvokeRepeating("SpawnSphere", 2.0f, rate);
+        sphereRenderer = sphere.GetComponentInChildren<Renderer>();
+        InvokeRepeating("SpawnSphere", initialDelay, rate);
     }
 
-    void SpawnSphere()
+    private void SpawnSphere()
     {
-        if (numSpheres > maxSpheres)
-        {
-            return;
+        if (numSpheres > maxSpheres) return;
 
-        }
-        Vector3 randomVector = Random.insideUnitSphere * overallSize;
-        float dist = Vector3.Distance(parent.position, randomVector);
-        if (dist < minSize)
-        {
-            return;
-        }
+        Vector3 spawnPosition = GetValidSpawnPosition();
 
         GameObject s = Instantiate(sphere);
-        s.transform.position = parent.position + randomVector;
+        s.transform.position = parent.position + spawnPosition;
         s.transform.localScale = Vector3.zero;
 
         Rotate rotation = s.GetComponent<Rotate>();
@@ -44,5 +44,31 @@ public class SpawnSpheres : MonoBehaviour
         scaleIn.length = Random.Range(0.5f, 10.0f);
 
         numSpheres++;
+    }
+
+    private Vector3 GetValidSpawnPosition()
+    {
+        bool isValidPosition = false;
+        Vector3 validPosition = new Vector3();
+        int numAttempts = 0;
+
+        while (!isValidPosition && numAttempts < maxAttemptsAtValidPosition)
+        {
+            numAttempts++;
+            Vector3 randomVector = Random.insideUnitSphere * overallSize;
+            isValidPosition = CheckValidPosition(randomVector);
+            if (isValidPosition)
+            {
+                validPosition = randomVector;
+            }
+        }
+        return validPosition;
+    }
+
+    private bool CheckValidPosition(Vector3 pos)
+    {
+        bool greaterThanMinimumDistance = Vector3.Distance(parent.position, pos) > minSize;
+        bool aboveFloor = pos.y > floor.position.y + sphereRenderer.bounds.size.y + paddingAboveFloor;
+        return greaterThanMinimumDistance && aboveFloor;
     }
 }

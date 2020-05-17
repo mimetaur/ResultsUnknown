5b5b01af82cf45269829ad8c4e56645f128ad7fc
Nathan Koch <nathanrkoch@gmail.com>
2020-05-17 14:22:22 -0400

    at parity with an audioclip based approach

--- Assets/Scripts/SphereAudioController.cs
+++ Assets/Scripts/SphereAudioController.cs
@@ -9,6 +9,14 @@ public class SphereAudioController : MonoBehaviour
     private ChuckSubInstance chuck;
     private AudioSource source;
 
+    public float baseRate = 1.0f;
+    public float rateRangeDown = 0.5f;
+    public float rateRangeUp = 1.0f;
+    public float audioFadeDuration = 3.0f;
+
+    private float minVolume = 0f;
+    private float maxVolume = 1f;
+
     void Awake()
     {
         sc = GetComponent<SphereController>();
@@ -18,30 +26,77 @@ public class SphereAudioController : MonoBehaviour
 
     public void Play()
     {
-        source.volume = 1f;
-        chuck.RunCode(@"
-            SndBuf textureBuf => dac;
-            me.dir() + ""texture01.wav"" => textureBuf.read;
+        // rate of the audio file playback is driven by the speed at which the sphere rotates
+        float rate = GameUtils.Map(sc.GetSphereRotateAroundSpeedNormalized(), 0f, 1f, baseRate - rateRangeDown, baseRate + rateRangeUp);
 
-            // start at the beginning of the clip
-            0 => textureBuf.pos;
+        // gain of the audio file playback is driven by the size of the sphere
+        float gain = GameUtils.Map(sc.GetSphereSizeNormalized(), 0f, 1f, 0.5f, 1.0f);
 
-            // pass time so that the file plays
-            textureBuf.length() / textureBuf.rate() => now;
-        ");
+        chuck.SetRunning(true);
+        chuck.RunCode(string.Format(@"
+                SndBuf textureBuf => dac;
+                me.dir() + ""texture01.wav"" => textureBuf.read;
 
-        // chuck.RunCode(@"
-        // 	TriOsc spatialOsc => dac;
-        // 	while( true )
-        // 	{
-        // 		Math.random2f( 300, 1000 ) => spatialOsc.freq;
-        // 		50::ms => now;
-        // 	}
-        // ");
+                // start at the beginning of the clip
+                0 => textureBuf.pos;
+
+                {0} => textureBuf.rate;
+
+                // pass time so that the file plays
+                textureBuf.length() / textureBuf.rate() => now;
+            ", rate, gain));
+        StartCoroutine(FadeAudio(FadeDirection.Up, DoNothingOnComplete));
     }
 
     public void Stop()
     {
+
+        StartCoroutine(FadeAudio(FadeDirection.Down, StopAudioOnComplete));
+    }
+
+    // callback action when fade coroutine is complete
+    private void DoNothingOnComplete()
+    {
+        // do nothing
+    }
+
+    // callback action when fade coroutine is complete
+    private void StopAudioOnComplete()
+    {
         source.volume = 0f;
+        chuck.SetRunning(false);
+    }
+
+    private IEnumerator FadeAudio(FadeDirection direction, System.Action onComplete)
+    {
+        const int numFadeSteps = 100;
+        float stepAmount = 1f / numFadeSteps;
+
+        float easeSpeed = audioFadeDuration / numFadeSteps;
+        float easeStart = minVolume;
+        float easeGoal = maxVolume;
+
+        if (direction == FadeDirection.Down)
+        {
+            easeStart = maxVolume;
+            easeGoal = minVolume;
+        }
+
+        for (float pct = 0; pct <= 1f; pct += stepAmount)
+        {
+            float volumeLevel;
+            if (direction == FadeDirection.Down)
+            {
+                volumeLevel = Mathfx.Sinerp(easeStart, easeGoal, pct);
+            }
+            else
+            {
+                volumeLevel = Mathfx.Coserp(easeStart, easeGoal, pct);
+            }
+            source.volume = volumeLevel;
+
+            yield return new WaitForSeconds(easeSpeed);
+        }
+        onComplete();
     }
 }

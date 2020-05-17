2e3bcc4e92b5ca009284cd6adc583eeb939779a0
Nathan Koch <nathanrkoch@gmail.com>
2020-05-17 13:20:38 -0400

    chuck install works, sub instances attached to sphere prefabs

---
+++ Assets/Chunity/Scripts/Examples/ChunityExampleRunCode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunityExampleRunCode : MonoBehaviour
+{
+	// This example runs a ChucK script every time
+	// the space bar is pressed.
+
+	ChuckSubInstance myChuck;
+
+	// Use this for initialization
+	void Start()
+	{
+		myChuck = GetComponent<ChuckSubInstance>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+#if UNITY_IOS
+		for (int i = 0; i < Input.touchCount; ++i)
+		{
+			if (Input.GetTouch(i).phase == TouchPhase.Began)
+			{
+#else
+		if( Input.GetKeyDown( "space" ) )
+		{
+#endif
+			// rotate my cube's transform
+			transform.Rotate( new Vector3( 0, 15, 5 ) );
+
+			// play a chuck script
+			myChuck.RunCode( @"
+				SndBuf buffy => dac;
+				""special:dope"" => buffy.read;
+				buffy.length() => now;		
+	
+			" );
+		}
+#if UNITY_IOS
+		}
+#endif
+
+	}
+}
--- Assets/Scripts/SphereAudioController.cs
+++ Assets/Scripts/SphereAudioController.cs
@@ -2,91 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource)), RequireComponent(typeof(SphereController))]
+[RequireComponent(typeof(SphereController)), RequireComponent(typeof(ChuckSubInstance)), RequireComponent(typeof(AudioSource))]
 public class SphereAudioController : MonoBehaviour
 {
-    public float basePitch = 1.0f;
-    public float pitchRangeDown = 0.5f;
-    public float pitchRangeUp = 1.0f;
-    public float audioFadeDuration = 3.0f;
-
     private SphereController sc;
-    private AudioSource audioSource;
-    private float minVolume = 0f;
-    private float maxVolume = 1f;
+    private ChuckSubInstance chuck;
+    private AudioSource source;
 
     void Awake()
     {
         sc = GetComponent<SphereController>();
-        audioSource = GetComponent<AudioSource>();
+        chuck = GetComponent<ChuckSubInstance>();
+        source = GetComponent<AudioSource>();
     }
 
     public void Play()
     {
-        audioSource.pitch = GameUtils.Map(sc.GetSphereRotateAroundSpeedNormalized(), 0f, 1.0f, basePitch - pitchRangeDown, basePitch + pitchRangeUp);
-
-        if (!audioSource.isPlaying)
-        {
-            audioSource.Play();
-            StartCoroutine(FadeAudio(FadeDirection.Up, DoNothingOnComplete));
-        }
-
+        source.volume = 1f;
+        chuck.RunCode(@"
+			TriOsc spatialOsc => dac;
+			while( true )
+			{
+				Math.random2f( 300, 1000 ) => spatialOsc.freq;
+				50::ms => now;
+			}
+		");
     }
 
     public void Stop()
     {
-        if (audioSource.isPlaying)
-        {
-            StartCoroutine(FadeAudio(FadeDirection.Down, StopAudioOnComplete));
-        }
-
-    }
-
-    // callback action when fade coroutine is complete
-    private void DoNothingOnComplete()
-    {
-        // do nothing
-    }
-
-    // callback action when fade coroutine is complete
-    private void StopAudioOnComplete()
-    {
-        if (audioSource.isPlaying)
-        {
-            audioSource.Stop();
-        }
-    }
-
-    private IEnumerator FadeAudio(FadeDirection direction, System.Action onComplete)
-    {
-        const int numFadeSteps = 100;
-        float stepAmount = 1f / numFadeSteps;
-
-        float easeSpeed = audioFadeDuration / numFadeSteps;
-        float easeStart = minVolume;
-        float easeGoal = maxVolume;
-
-        if (direction == FadeDirection.Down)
-        {
-            easeStart = maxVolume;
-            easeGoal = minVolume;
-        }
-
-        for (float pct = 0; pct <= 1f; pct += stepAmount)
-        {
-            float volumeLevel;
-            if (direction == FadeDirection.Down)
-            {
-                volumeLevel = Mathfx.Sinerp(easeStart, easeGoal, pct);
-            }
-            else
-            {
-                volumeLevel = Mathfx.Coserp(easeStart, easeGoal, pct);
-            }
-            audioSource.volume = volumeLevel;
-
-            yield return new WaitForSeconds(easeSpeed);
-        }
-        onComplete();
+        source.volume = 0f;
     }
 }
